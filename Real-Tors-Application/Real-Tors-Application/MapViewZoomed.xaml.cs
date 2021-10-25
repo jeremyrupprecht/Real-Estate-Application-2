9f0af7d997d5befd917fa87cd8d121ca319f4add
FabulousFritz <jasminenguyen8169@gmail.com>
2021-10-25 15:11:12 -0600

    working on zoomed map

---
+++ Real-Tors-Application/Real-Tors-Application/MapViewZoomed.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Real_Tors_Application
+{
+    /// <summary>
+    /// Interaction logic for MapViewZoomed.xaml
+    /// </summary>
+    public partial class MapViewZoomed : Page
+    {
+        public MapViewZoomed()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_HomePage_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("HomePage.xaml", UriKind.Relative));
+        }
+
+        private void Btn_Map_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("MapView.xaml", UriKind.Relative));
+        }
+
+        private void Btn_ListView_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("ListView.xaml", UriKind.Relative));
+        }
+    }
+
+}
