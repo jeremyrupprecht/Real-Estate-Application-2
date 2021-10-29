172e8ac7507789f84a6a3a8483d7a673c569434d
Thanh Hien Nguyen-Mai <47198098+FabulousFritz@users.noreply.github.com>
2021-10-29 01:55:58 -0600

    Minor fixes

--- Real-Tors-Application/Real-Tors-Application/MapViewZoomed2Hovered.xaml.cs
+++ Real-Tors-Application/Real-Tors-Application/MapViewZoomed2Hovered.xaml.cs
@@ -24,5 +24,30 @@ namespace Real_Tors_Application
         {
             InitializeComponent();
         }
+
+        private void Btn_Map_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("MapViewUnZoomed.xaml", UriKind.Relative));
+        }
+
+        private void Btn_ListView_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("ListView.xaml", UriKind.Relative));
+        }
+
+        private void homeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("HomePage.xaml", UriKind.Relative));
+        }
+
+        private void navLogInButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("LoginView.xaml", UriKind.Relative));
+        }
+
+        private void signUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("SignupView.xaml", UriKind.Relative));
+        }
     }
 }
