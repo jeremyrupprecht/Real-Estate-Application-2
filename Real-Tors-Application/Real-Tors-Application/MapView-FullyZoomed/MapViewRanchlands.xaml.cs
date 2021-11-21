bc1cab339d6febfbd98376273793c3732a6ee2f0
Thanh Hien Nguyen-Mai <47198098+FabulousFritz@users.noreply.github.com>
2021-11-21 04:13:05 -0700

    more map stuff

--- Real-Tors-Application/Real-Tors-Application/MapView-FullyZoomed/MapViewRanchlands.xaml.cs
+++ Real-Tors-Application/Real-Tors-Application/MapView-FullyZoomed/MapViewRanchlands.xaml.cs
@@ -24,5 +24,87 @@ namespace Real_Tors_Application.MapView_FullyZoomed
         {
             InitializeComponent();
         }
+
+        private void Btn_Map_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("MapViewZoomed2Hovered.xaml", UriKind.Relative));
+        }
+
+        private void Btn_Zoomout_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("MapViewUnZoomed.xaml", UriKind.Relative));
+        }
+
+        private void Btn_ListView_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("ListView.xaml", UriKind.Relative));
+        }
+
+        private void homeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("HomePage.xaml", UriKind.Relative));
+        }
+
+        private void navLogInButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("LoginView.xaml", UriKind.Relative));
+        }
+
+        private void signUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("SignupView.xaml", UriKind.Relative));
+        }
+
+        private void toggleTypes(object sender, RoutedEventArgs e)
+        {
+            if (HomeTypesSelect.Visibility == Visibility.Collapsed)
+            {
+                HomeTypesSelect.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                HomeTypesSelect.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void OpenAmenities_Click(object sender, RoutedEventArgs e)
+        {
+            if (AmenTypesSelect.Visibility == Visibility.Collapsed)
+            {
+                AmenTypesSelect.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                AmenTypesSelect.Visibility = Visibility.Collapsed;
+            }
+
+        }
+
+        private void toggleNeigh(object sender, RoutedEventArgs e)
+        {
+            if (NeighTypesSelect.Visibility == Visibility.Collapsed)
+            {
+                NeighTypesSelect.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                NeighTypesSelect.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void ToggleFilter(object sender, RoutedEventArgs e)
+        {
+            if (FilterPanel.Visibility == Visibility.Collapsed)
+            {
+                FilterPanel.Visibility = Visibility.Visible;
+
+            }
+            else
+            {
+                FilterPanel.Visibility = Visibility.Collapsed;
+
+            }
+
+        }
     }
 }
